06a3275cf320bd82ae33693aac77d00f8740d37d
PritamBiswas489 <pritam.biswas489@gmail.com>
2025-07-17 22:34:57 +0530

    00225588

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename);
 
 // Resolve the path to the service account key
 const serviceAccountPath = join(__dirname, '../../firebase/development-service-account.json');
+console.log(serviceAccountPath)
 
 // Read and parse the key
 const serviceAccount = JSON.parse(readFileSync(serviceAccountPath));
