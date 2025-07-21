9ac059909a3dfea96721edb42d6db58b23af09bf
PritamBiswas489 <pritam.biswas489@gmail.com>
2025-07-21 20:57:44 +0530

    21-07-2025-08-57-pm

--- src/services/wallet.service.js
+++ src/services/wallet.service.js
@@ -4,7 +4,7 @@ import * as Sentry from "@sentry/node";
 import { getPeleCardCurrencyNumber, amountUptotwoDecimalPlaces } from "../libraries/utility.js";
 import CurrencyService from "./currency.service.js";
 import SettingsService from "./settings.service.js";
-const { WalletPelePayment, Op, User, UserWallet, WalletTransaction, Transfer } = db;
+const { WalletPelePayment, Op, User, UserWallet, WalletTransaction, Transfer, TransferRequests } = db;
 
 export default class WalletService {
   static async updateUserWalletBalanceAfterPayment(
