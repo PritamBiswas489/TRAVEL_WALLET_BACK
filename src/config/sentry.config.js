b293642f113595e7b980936b704ecb82ba2abfba
PritamBiswas489 <pritam.biswas489@gmail.com>
2025-12-30 21:24:38 +0530

    301220250924pm

--- src/config/sentry.config.js
+++ src/config/sentry.config.js
@@ -15,7 +15,7 @@ export function initializeSentry(dsn) {
 		],
 		// Add Tracing by setting tracesSampleRate
 		// We recommend adjusting this value in production
-		tracesSampleRate: 1.0,
+		tracesSampleRate: 0,
 		// Set sampling rate for profiling
 		// This is relative to tracesSampleRate
 		profilesSampleRate: 1.0,
