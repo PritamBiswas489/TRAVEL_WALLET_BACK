8283af94f4cb4b04724fe50355ca0ad78407b43a
PritamBiswas489 <pritam.biswas489@gmail.com>
2026-08-14 16:27:34 +0530

    140820260427pm

--- src/config/paymentCurrencies.js
+++ src/config/paymentCurrencies.js
@@ -1,21 +1,62 @@
- 
-
 export const paymentCurrencies = {
-    THB: {
-        symbol: "฿",
-        name: "Thai Baht",
-    },
-    PHP: {
-        symbol: "₱",
-        name: "Philippine Peso"
-    },
-    VND: {
-        symbol: "₫",
-        name: "Vietnamese Dong"
-    },
-    KHR: {
-        symbol: "៛",
-        name: "Cambodian Riel"
-    },
-    
+  THB: {
+    symbol: "฿",
+    name: "Thai Baht",
+  },
+  PHP: {
+    symbol: "₱",
+    name: "Philippine Peso",
+  },
+  VND: {
+    symbol: "₫",
+    name: "Vietnamese Dong",
+  },
+  KHR: {
+    symbol: "៛",
+    name: "Cambodian Riel",
+  },
+  USD: {
+    symbol: "$",
+    name: "United States Dollar",
+  },
+  ILS: {
+    symbol: "₪",
+    name: "Israeli New Shekel",
+  },
+  EUR: {
+    symbol: "€",
+    name: "Euro",
+  },
+  AUD: {
+    symbol: "A$",
+    name: "Australian Dollar",
+  },
+  SGD: {
+    symbol: "S$",
+    name: "Singapore Dollar",
+  },
+  HKD: {
+    symbol: "HK$",
+    name: "Hong Kong Dollar",
+  },
+  GBP: {
+    symbol: "£",
+    name: "British Pound Sterling",
+  },
+  JPY: {
+    symbol: "¥",
+    name: "Japanese Yen",
+  },
+  CAD: {
+    symbol: "C$",
+    name: "Canadian Dollar",
+  },
+  NZD: {
+    symbol: "NZ$",
+    name: "New Zealand Dollar",
+  },
+  CHF: {
+    symbol: "CHF",
+    name: "Swiss Franc",
+  },
 };
--- src/config/walletCurrencies.js
+++ src/config/walletCurrencies.js
@@ -1,14 +1,62 @@
 export const walletCurrencies = {
-  "USD": {
-    "symbol": "$",
-    "name": "United States Dollar"
+  USD: {
+    symbol: "$",
+    name: "United States Dollar",
   },
-  "ILS": {
-    "symbol": "₪",
-    "name": "Israeli New Shekel"
+  ILS: {
+    symbol: "₪",
+    name: "Israeli New Shekel",
   },
-  "EUR": {
-    "symbol": "€",
-    "name": "Euro"
-  }
+  EUR: {
+    symbol: "€",
+    name: "Euro",
+  },
+  AUD: {
+    symbol: "A$",
+    name: "Australian Dollar",
+  },
+  SGD: {
+    symbol: "S$",
+    name: "Singapore Dollar",
+  },
+  HKD: {
+    symbol: "HK$",
+    name: "Hong Kong Dollar",
+  },
+  GBP: {
+    symbol: "£",
+    name: "British Pound Sterling",
+  },
+  JPY: {
+    symbol: "¥",
+    name: "Japanese Yen",
+  },
+  CAD: {
+    symbol: "C$",
+    name: "Canadian Dollar",
+  },
+  NZD: {
+    symbol: "NZ$",
+    name: "New Zealand Dollar",
+  },
+  CHF: {
+    symbol: "CHF",
+    name: "Swiss Franc",
+  },
+  THB: {
+    symbol: "฿",
+    name: "Thai Baht",
+  },
+  PHP: {
+    symbol: "₱",
+    name: "Philippine Peso",
+  },
+  VND: {
+    symbol: "₫",
+    name: "Vietnamese Dong",
+  },
+  KHR: {
+    symbol: "៛",
+    name: "Cambodian Riel",
+  },
 };
